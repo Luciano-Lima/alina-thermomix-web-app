d9aeb64d018f43905e590aa1a17e8b2e7f8bf750
Luciano Lima <luciano_lima@Lucianos-MacBook-Pro.local>
2024-10-13 13:50:33 +0100

    Create a function to show/hide btns on video plays

--- static/script/script.js
+++ static/script/script.js
@@ -1,4 +1,4 @@
-const scrollOffset = 100;
+const scrollOffset = 20;
 const scrollElements = document.querySelectorAll('.js-scroll')
 
 const elementInView = (el, offset = 0) => {
@@ -18,17 +18,47 @@ const hideScrollElement = (element) => {
     element.classList.remove('scrolled');
 }
 
+//only show the element once when it comes into view
 const handleScrollAnimation = () => {
     scrollElements.forEach((el) => {
-
         if (elementInView(el, scrollOffset)) {
             displayScrollElement(el);
         } else {
-            hideScrollElement(el);
+            hideScrollElement(el)
         }
     })
 }
 
+//Add a Debunce to the scroll envent to improve performance
+
+const debounce = (func, delay = 20) => {
+    let timer;
+    return () => {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        func()
+      }, delay)
+    }
+  }
+
 document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('scroll', handleScrollAnimation)
+    window.addEventListener('scroll', debounce(handleScrollAnimation))
+    handleScrollAnimation()  // Ensure animations trigger on page load in case any elements are already in view
+})
+
+// Show / Hide button when video plays
+
+const video = document.getElementById('thermomixVideo')
+const buttonsContainer = document.getElementById('thermomixButtons')
+
+video.addEventListener('play', () => {
+    buttonsContainer.style.display = 'none'
+})
+
+video.addEventListener('pause', () => {
+    buttonsContainer.style.display = 'inline-flex'
+})
+
+video.addEventListener('ended', () => {
+    buttonsContainer.style.display = 'inline-flex'
 })

c20924f777f85c6b330585613fa37790ee0d9a37
Luciano Lima <luciano_lima@Lucianos-MacBook-Pro.local>
2024-11-11 09:20:45 +0000

    Adding formspree to contact form

--- static/script/script.js
+++ static/script/script.js
@@ -33,20 +33,20 @@ const handleScrollAnimation = () => {
 const debounce = (func, delay = 20) => {
     let timer;
     return () => {
-      clearTimeout(timer)
-      timer = setTimeout(() => {
-        func()
-      }, delay)
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            func()
+        }, delay)
     }
-  }
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', debounce(handleScrollAnimation))
     handleScrollAnimation()  // Ensure animations trigger on page load in case any elements are already in view
-    const readMoreBtn =  document.querySelector('.read-more')
+    const readMoreBtn = document.querySelector('.read-more')
     const toggleContent = document.querySelector('.toggle-content')
 
-    if(readMoreBtn && toggleContent ){
+    if (readMoreBtn && toggleContent) {
         readMoreBtn.addEventListener('click', loadContent)
     }
 })
@@ -76,3 +76,33 @@ function loadContent() {
     this.innerText = toggleContent.classList.contains('show-content') ? 'Aproape' : 'Citeşte Mai Mult';
 
 }
+
+
+
+// Formspree to handle redicted after form submition
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+        method: 'POST',
+        body: formData,
+        headers: { 'Accept': 'application/json' }
+    })
+        .then(response => {
+            if (response.ok) {
+                alert('Thank you! Your message has been sent.');
+                form.reset(); // Clear the form
+            } else {
+                alert('Oops! There was a problem submitting your form.');
+            }
+        })
+        .catch(error => {
+            alert('Oops! There was a problem submitting your form.');
+            console.error('Error:', error);
+        });
+});
+

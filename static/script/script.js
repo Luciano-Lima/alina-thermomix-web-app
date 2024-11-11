ca5f4848c22d6fbcc4f1319ed137194c6f188792
Luciano Lima <luciano_lima@Lucianos-MacBook-Pro.local>
2024-11-11 09:39:08 +0000

    testing form

--- static/script/script.js
+++ static/script/script.js
@@ -94,10 +94,11 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
     })
         .then(response => {
             if (response.ok) {
-                alert('Thank you! Your message has been sent.');
+                window.location.href = 'index.html';
                 form.reset(); // Clear the form
             } else {
-                alert('Oops! There was a problem submitting your form.');
+                console.error('Form submission failed:', response.status, response.statusText);
+            alert('Oops! There was a problem submitting your form.');
             }
         })
         .catch(error => {
